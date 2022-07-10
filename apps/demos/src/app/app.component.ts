3e2be406df9ea0fc1b05ae1670bef77220b9b016
Ilya <iluhamishkin@gmail.com>
2022-07-10 20:48:54 +0200

    Initialize project

---
+++ apps/demos/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rx-directives-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'demos';
+}
